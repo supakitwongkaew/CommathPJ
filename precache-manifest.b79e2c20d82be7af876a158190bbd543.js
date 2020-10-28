e68230bfdba81a8eabafee9c67e7d5c5e16dc3ad
supakitwongkaew <supakit.wo.61@ubu.ac.th>
2020-10-29 00:03:05 +0700

    Updates

---
+++ precache-manifest.b79e2c20d82be7af876a158190bbd543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a760d144fd8a7c57e7e57187ff40156",
+    "url": "/CommathPJ/index.html"
+  },
+  {
+    "revision": "1f7c10ef8700c2434f22",
+    "url": "/CommathPJ/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6f35607825bca2854bdb",
+    "url": "/CommathPJ/static/css/main.313a00a9.chunk.css"
+  },
+  {
+    "revision": "1f7c10ef8700c2434f22",
+    "url": "/CommathPJ/static/js/2.66ddbb04.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CommathPJ/static/js/2.66ddbb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f35607825bca2854bdb",
+    "url": "/CommathPJ/static/js/main.99dc9e45.chunk.js"
+  },
+  {
+    "revision": "f54f0aece36c00742667",
+    "url": "/CommathPJ/static/js/runtime-main.972e7eed.js"
+  }
+]);
